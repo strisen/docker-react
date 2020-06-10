04d5bcf1f1645b0c20fbec58cf73472c594a2ad4
strisen <signum.aeterna@gmail.com>
2020-06-10 17:19:38 +0800

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn react on feature branch!
         </a>
       </header>
     </div>
